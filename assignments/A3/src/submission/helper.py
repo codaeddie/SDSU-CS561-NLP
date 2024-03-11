6ed7b6c6b37934598c323b7bf8ea5e415df0f174
eddie <eddiecoda24@gmail.com>
2024-03-11 04:57:05 -0700

    Adding A3

---
+++ assignments/A3/src/submission/helper.py
@@ -0,0 +1,146 @@
+from .model import GPT
+from .dataset import NameDataset, CharCorruptionDataset
+from .trainer import Trainer, TrainerConfig
+
+import torch
+import random
+
+random.seed(0)
+device = torch.cuda.current_device() if torch.cuda.is_available() else 'cpu'
+
+
+def initialize_vanilla_model(mconf):
+    attention_model = None
+    ### TODO:
+    ### [part c]: Make some model here
+
+    ### START CODE HERE
+    attention_model = GPT(mconf).to(device)
+    ### END CODE HERE
+    return attention_model
+
+
+def initialize_synthesizer_model(mconf):
+    attention_model = None
+    ### TODO
+    ### [part g]: Make some other model here
+
+    ### START CODE HERE
+    ### END CODE HERE
+    return attention_model
+
+
+def finetune(reading_params_path, finetune_corpus_path, pretrain_dataset, block_size, model):
+    ### TODO:
+    finetune_corpus_path = open(finetune_corpus_path).read()
+    finetune_dataset = NameDataset(finetune_corpus_path, pretrain_dataset)
+    ### [part c] [part f]:
+    ### - Given:
+    ###     1. A finetuning corpus specified in finetune_corpus_path
+    ###     2. A path reading_params_path containing pretrained model
+    ###         parameters, or None if finetuning without a pretrained model
+    ###     3. An output path writing_params_path for the model parameters
+    ### - Goals:
+    ###     1. If reading_params_path is specified, load these parameters
+    ###         into the model
+    ###     2. Finetune the model on this corpus
+    ###
+    ### - Make sure to use the following hyperparameters:
+    ###     Hyperparameters for finetuning WITHOUT a pretrained model:
+    ###         max_epochs=75
+    ###         batch_size=128
+    ###         learning_rate=6e-4
+    ###         lr_decay=True
+    ###         warmup_tokens=512*20
+    ###         final_tokens=200*len(pretrain_dataset)*block_size
+    ###         num_workers=4
+    ###     Hyperparameters for finetuning WITH a pretrained model:
+    ###         max_epochs=10
+    ###         batch_size=256
+    ###         learning_rate=6e-4
+    ###         lr_decay=True
+    ###         warmup_tokens=512*20
+    ###         final_tokens=200*len(pretrain_dataset)*block_size
+    ###         num_workers=4
+    ###
+    ###
+    ### Note: Please use torch.load(reading_params_path, map_location=torch.device('cpu')) to load pretrained model
+
+    if reading_params_path is not None:
+        # Finetuning WITH a pretrained model
+        tconf = TrainerConfig(
+            max_epochs=10,
+            batch_size=256,
+            learning_rate=6e-4,
+            lr_decay=True,
+            warmup_tokens=512 * 20,
+            final_tokens=200 * len(pretrain_dataset) * block_size,
+            num_workers=4
+        )
+        model.load_state_dict(torch.load(reading_params_path, map_location=torch.device('cpu')))
+    else:
+        # Finetuning WITHOUT a pretrained model
+        tconf = TrainerConfig(
+            max_epochs=75,
+            batch_size=128,
+            learning_rate=6e-4,
+            lr_decay=True,
+            warmup_tokens=512 * 20,
+            final_tokens=200 * len(pretrain_dataset) * block_size,
+            num_workers=4
+        )
+
+    trainer_obj = Trainer(model, finetune_dataset, None, tconf)
+    trainer_obj.train()
+
+    return tconf, trainer_obj
+
+
+def pretrain(pretrain_dataset, block_size, model):
+    ### TODO:
+    ### [part f]:
+    ### - Given:
+    ###     1. A corpus specified in pretrain_corpus_path
+    ### - Goals:
+    ###     1. Pretrain the model on this corpus
+    ###
+    ### - Make sure to use the following hyperparameters for pretraining:
+    ###     max_epochs=650
+    ###     batch_size=128
+    ###     learning_rate=6e-3
+    ###     lr_decay=True
+    ###     warmup_tokens=512*20
+    ###     final_tokens=200*len(pretrain_dataset)*block_size
+    ###     num_workers=4
+
+    trainer_obj = None  # Trainer object (see trainer.py for more details)
+    tconf = None  # TrainerConfig object (see trainer.py for more details)
+
+    tconf = TrainerConfig(
+        max_epochs=650,
+        batch_size=128,
+        learning_rate=6e-3,
+        lr_decay=True,
+        warmup_tokens=512*20,
+        final_tokens=200*len(pretrain_dataset)*block_size,
+        num_workers=4
+    )
+
+    trainer_obj = Trainer(model, pretrain_dataset, None, tconf)
+
+    return tconf, trainer_obj
+
+
+def train(model, writing_params_path, trainer_obj):
+    for epoch in range(trainer_obj.config.max_epochs):
+        # Train for one epoch
+        trainer_obj.train()
+
+        # Print GPU memory usage
+        print(f"After epoch {epoch}, GPU memory allocated: {torch.cuda.memory_allocated() / 1024 ** 2} MB")
+        print(f"After epoch {epoch}, GPU memory reserved: {torch.cuda.memory_reserved() / 1024 ** 2} MB")
+
+    # Save the model parameters
+    torch.save(model.state_dict(), writing_params_path)
+    ### END CODE HERE
+    return
